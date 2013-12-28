9c67382cf5317600ecc54f82b416876045a71ae1
yterajima <terra@e2esound.com>
2013-12-28 23:26:45 +0900

    ReOrganize gemspec and Gemfile
    - middleman-slim.gemspec has all gem dependencies.
    - Gemfile is Simplified.
    - Gemfile.tt is added some gems.
    - Remove some lines from env.rb because these lines are not used.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,1 @@
-ENV["TEST"] = "true"
-ENV["AUTOLOAD_SPROCKETS"] = "false"
-
-require 'simplecov'
-SimpleCov.root(File.expand_path(File.dirname(__FILE__) + '/../..'))
-
-require 'middleman-core'
 require 'middleman-core/step_definitions'
