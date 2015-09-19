8e63fd5775609aacc531aa26771ce5c944a6efa6
yterajima <terra@e2esound.com>
2015-09-20 01:54:23 +0900

    remove unnecessary files

--- lib/middleman-slim.rb
+++
@@ -1,5 +0,0 @@
-require 'middleman-core'
-require 'middleman-slim/version'
-require 'middleman-slim/template'
-
-Middleman::Templates.register :slim, Middleman::Slim::Template
--- lib/middleman-slim/version.rb
+++
@@ -1,5 +0,0 @@
-module Middleman
-  module Slim
-    VERSION = "0.2.2"
-  end
-end
